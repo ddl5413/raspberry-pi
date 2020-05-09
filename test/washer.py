c252ff90092d337198a61bc589bea612c05c6f29
ddl5413 <ddl5@163.com>
2020-05-09 18:27:47 +0800

    test

--- test/washer.py
+++ test/washer.py
@@ -6,7 +6,20 @@ GPIO.setwarnings(False)
 GPIO.setup(7, GPIO.OUT)
 GPIO.setup(11, GPIO.OUT)
 
-GPIO.output(7, 1)
-GPIO.output(11, 0)
-time.sleep(10)
+for i in range(0, 4):
+    GPIO.output(7, 1)
+    GPIO.output(11, 0)
+    time.sleep(10)
+
+    GPIO.output(7, 0)
+    GPIO.output(11, 0)
+    time.sleep(4)
+
+    GPIO.output(7, 0)
+    GPIO.output(11, 1)
+    time.sleep(10)
+
+    GPIO.output(7, 0)
+    GPIO.output(11, 0)
+    time.sleep(4)
 GPIO.cleanup()
