dd1e02bbe583cabdea1c3158d7072c5be9cc709c
ddl5413 <ddl5@163.com>
2020-05-24 15:15:18 +0800

    test

---
+++ test/stepping-motor-right.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import time
+
+def step(i1,i2,i3,i4):
+	GPIO.output(7,i1)
+	GPIO.output(11,i2)
+	GPIO.output(13,i3)
+	GPIO.output(15,i4)
+	time.sleep(0.002)
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(7, GPIO.OUT)
+GPIO.setup(11, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(15, GPIO.OUT)
+for i in range(0,500):
+	step(0,0,0,1)
+	step(0,0,1,0)
+	step(0,1,0,0)
+	step(1,0,0,0)
+GPIO.cleanup()
+
