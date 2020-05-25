4620d6674584ed229b04cb2c579c23165ce7c2fc
ddl5413 <ddl5@163.com>
2020-05-25 20:27:24 +0800

    test

--- test/forward.py
+++ test/forward.py
@@ -3,16 +3,16 @@ import time
 
 
 def forward():
-    GPIO.output(35, 1)
-    GPIO.output(37, 0)
+    GPIO.output(12, 1)
+    GPIO.output(16, 0)
     time.sleep(20)
 
 
 def main():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setwarnings(False)
-    GPIO.setup(35, GPIO.OUT)
-    GPIO.setup(37, GPIO.OUT)
+    GPIO.setup(12, GPIO.OUT)
+    GPIO.setup(16, GPIO.OUT)
     forward()
     GPIO.cleanup()
 
