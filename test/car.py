f54876f0a582bef12e1958237999fbb921f01533
ddl5413 <ddl5@163.com>
2020-05-27 22:29:27 +0800

    test

--- test/car.py
+++ test/car.py
@@ -1,14 +1,10 @@
-import RPi.GPIO as GPIO
-import time
+
 
 
 class Car:
 
     def __init__(self):
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setwarnings(False)
-        GPIO.setup(12, GPIO.OUT)
-        GPIO.setup(16, GPIO.OUT)
+
         print('init')
 
     def turn_left(self):
@@ -18,12 +14,10 @@ class Car:
         print('right')
 
     def forward(self):
-        GPIO.output(12, 1)
-        GPIO.output(16, 0)
+
         print('forward')
 
 
     def stop(self):
-        GPIO.output(12, 0)
-        GPIO.output(16, 0)
+
         print('stop')
