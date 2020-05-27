bd35fe70d5461b0d05a66ed693f256d179a963c0
ddl5413 <ddl5@163.com>
2020-05-27 22:34:44 +0800

    test

--- test/car_control.py
+++ test/car_control.py
@@ -1,6 +1,6 @@
+import RPi.GPIO as GPIO
 import web
 import time
-from car import Car
 
 urls = ('/(.*)', 'Action')
 
@@ -9,9 +9,15 @@ class Action:
     signal = False
     count = 0
 
+    def __init__(self):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(False)
+        GPIO.setup(12, GPIO.OUT)
+        GPIO.setup(16, GPIO.OUT)
+
     def GET(self, action):
         if action == 'start':
-            Car.forward()
+            Action.forward()
             Action.signal = True
             while Action.signal:
                 Action.count += 1
@@ -21,7 +27,23 @@ class Action:
                     break
         if action == 'stop':
             Action.signal = False
-            Car.stop()
+            Action.stop()
+
+    def urn_left(self):
+        print('left')
+
+    def turn_right(self):
+        print('right')
+
+    def forward(self):
+        GPIO.output(12, 1)
+        GPIO.output(16, 0)
+        print('forward')
+
+    def stop(self):
+        GPIO.output(12, 0)
+        GPIO.output(16, 0)
+        print('stop')
 
 
 if __name__ == '__main__':
